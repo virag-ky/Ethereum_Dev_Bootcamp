dc2224903b5e6d65c1a836e5cca4195c2f397847
virag-ky <vkormoczy@gmail.com>
2025-09-13 23:21:04 +0300

    Add the functions to get the public key, recover the public key, and get the address from key

--- lesson_01/script.js
+++ lesson_01/script.js
@@ -1,14 +1,45 @@
-import { keccak256 } from "ethereum-cryptography/keccak";
-import { utf8ToBytes, toHex } from "ethereum-cryptography/utils";
+import { secp256k1 } from "ethereum-cryptography/secp256k1.js";
+import { toHex, utf8ToBytes } from "ethereum-cryptography/utils.js";
+import { keccak256 } from "ethereum-cryptography/keccak.js";
+
+const PRIVATE_KEY =
+  "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e"; // just a practice string
+const publicKey = getThePublicKey(PRIVATE_KEY);
+const msg = "hello";
+const hashedMsg = messageToHash(msg);
+const signature = signTheMessage(hashedMsg);
+const recoveredPublicKey = recoverThePublicKey(hashedMsg);
 
 // 1.) Hash the message
 function messageToHash(message) {
-  // turn the message to an array of bytes
+  // turn message into array of bytes
   const bytes = utf8ToBytes(message);
-  // hash the message
-  const hashedMessage = toHex(keccak256(bytes));
+  // hash it
+  const hash = toHex(keccak256(bytes));
 
-  return hashedMessage;
+  return hash;
 }
 
 // 2.) Sign the message
+function signTheMessage(message) {
+  return secp256k1.sign(message, PRIVATE_KEY);
+}
+
+// 3.) Get the public key
+function getThePublicKey(privateKey) {
+  return secp256k1.getPublicKey(privateKey);
+}
+
+// 4.) Recover the public key
+function recoverThePublicKey(message) {
+  return signature.recoverPublicKey(message);
+}
+
+// 5.) Key to Address
+function getAddressFromKey(p_Key) {
+  // remove first byte then take the keccak hash of the rest
+  const modifiedKey = keccak256(p_Key.slice(1));
+  const addressBytes = modifiedKey.slice(-20);
+
+  return addressBytes;
+}
